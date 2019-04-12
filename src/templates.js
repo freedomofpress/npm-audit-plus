02d3ca19902fbc98b771a60524b3c41456a5f3e7
Harris Lapiroff <harris@chromamine.com>
2019-04-12 14:35:44 -0400

    Improve advisory rendering
    - Render actions more intelligently. "Review" actions are not run
      as a command. Major actions should be highlighted.
    - Add bullet points to lists

--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,15 @@
+const renderAction = action => {
+  // Use a set to dedupe resolved advisory IDs
+  const resolvedAdvisories = Array.from(new Set(action.resolves.map(x => x.id)))
+  if (action.isMajor) {
+    return `MAJOR: Run \`npm ${action.action} ${action.module}@${action.target}\` to resolve these advisories: ${resolvedAdvisories.join(', ')}`
+  } else if (action.action === 'install' || action.action === 'update') {
+    return `Run \`npm ${action.action} ${action.module}@${action.target}\` to resolve these advisories: ${resolvedAdvisories.join(', ')}`
+  } else if (action.action === 'review') {
+    return `Manually review \`${action.module}\` to resolve these advisories: ${resolvedAdvisories.join(', ')}`
+  }
+}
+
 // START TEMPLATE: Generate a human-readable title from an advisory object
 const advisoryTitle = advisoryObj => `${advisoryObj.severity.toUpperCase()}: ${advisoryObj.id} - ${advisoryObj.title}`
 // END TEMPLATE
@@ -8,9 +20,9 @@ const advisorySummary = advisoryObj => `${advisoryObj.overview}
 
 ${advisoryObj.recommendation}
 
-Version:   ${advisoryObj.findings[0].version}
-Path:      ${advisoryObj.findings[0].paths[0]}
-More info: ${advisoryObj.url}`
+- Version:   ${advisoryObj.findings[0].version}
+- Path:      ${advisoryObj.findings[0].paths[0]}
+- More info: ${advisoryObj.url}`
 // END TEMPLATE
 
 
@@ -18,11 +30,11 @@ More info: ${advisoryObj.url}`
 // to generate a summary of the audit
 const auditSummary = (actions, passThruIds, ignoredIds) => `Summary
 
-Advisories: ${passThruIds.length}
-Ignored:    ${ignoredIds.length}
+- Advisories: ${passThruIds.length}
+- Ignored:    ${ignoredIds.length}
 ${actions.length > 0 ? (
   '\nRecommended Actions\n\n' +
-  actions.map(action => `Run \`npm ${action.action} ${action.module}@${action.target}\` to resolve these advisories: ${action.resolves.map(x => x.id).join(', ')}`).join('\n')
+  actions.map(a => '- ' + renderAction(a)).join('\n')
 ): ''}`
 // END TEMPLATE
 
